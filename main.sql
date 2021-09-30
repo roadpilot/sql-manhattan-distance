5f3e711bd65f0decadb9972ebcba274c641c30aa
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-30 18:11:54 +0000

    explanation

--- main.sql
+++ main.sql
@@ -5,4 +5,7 @@ b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
 c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
 d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
 Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+
+Explanation:
+Manhattan Distance: The distance between two points measured along axes at right angles. In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|.
 */
