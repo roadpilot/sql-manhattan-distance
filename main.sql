54c0c12af754e7197ff8eee5e4569dc7494e744a
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-30 18:12:08 +0000

    solution

--- main.sql
+++ main.sql
@@ -9,3 +9,4 @@ Query the Manhattan Distance between points P1 and P2 and round it to a scale of
 Explanation:
 Manhattan Distance: The distance between two points measured along axes at right angles. In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|.
 */
+select round(abs(min(lat_n)-max(lat_n))+abs(min(long_w)-max(long_w)), 4) from station
